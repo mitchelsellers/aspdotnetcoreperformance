dace0974cb81b6aaefcd039697298a5f86006906
Mitchel Sellers <msellers@iowacomputergurus.com>
2023-06-02 12:04:01 -0500

    Initial project commit

---
+++ src/AspNetCorePerformanceDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCorePerformanceDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
