14a5aa10047ff45dc61f5d4f77d6bb23f63eb59d
Mitchel Sellers <msellers@iowacomputergurus.com>
2023-06-22 10:20:24 -0500

    Updated examples to add more

---
+++ src/AspNetCorePerformanceDemo/Models/IndexModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetCorePerformanceDemo.Models
+{
+    public class IndexModel
+    {
+        public DateTime CurrentTime { get; set; }
+        public DateTime CachedCurrentTime { get; set; }
+    }
+}
